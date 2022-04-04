19dd001f2734608caeb76247c8b9fcc18fd92084
Pajehome <abdel20069@gmail.com>
2022-04-04 18:46:40 +0200

    ditest

--- main.js
+++ main.js
@@ -4,9 +4,14 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+function active() {
+    let bgBlue = document.querySelector(".col");
+    bgBlue.classList.add("active");
+ }
 
+  
 
-function griglia() {
+function easy() {
      let container = document.querySelector('main');
      
 
@@ -14,11 +19,12 @@ function griglia() {
      row.setAttribute('class', 'row');
      console.log(row)
 
-     let colsCreate = creaColonne();
+     let colsCreate = colonneEasy();
      console.log(colsCreate);
      row.innerHTML = colsCreate;
      container.append(row);
      console.log(row);
+     return container
 }
 
 
@@ -27,7 +33,7 @@ function griglia() {
 const colEasy = 100
 
 
- function creaColonne() {
+ function colonneEasy() {
    let cols = '';
    let numeriusati = [];
    for(let i = 1; i <= colEasy; i++){ 
@@ -38,5 +44,76 @@ const colEasy = 100
   return cols
  }
 
- document.getElementById('generate').addEventListener('click', griglia);
+ document.getElementById('generate').addEventListener('click', easy);
+ document.getElementsByClassName("col").addEventListener("click", active);
  
+ function hard() {
+    let container = document.querySelector('main');
+    
+
+    let row = document.createElement('div');
+    row.setAttribute('class', 'row');
+    console.log(row)
+
+    let colsCreate = colonneHard();
+    console.log(colsCreate);
+    row.innerHTML = colsCreate;
+    container.append(row);
+    console.log(row);
+    return container
+}
+
+
+
+
+const colHard = 81
+
+
+function colonneHard() {
+  let cols = '';
+  let numeriusati = [];
+  for(let i = 1; i <= colHard; i++){ 
+     cols += `
+    <div class="col-1"> ${i}</div>
+    `;
+   }
+ return cols
+}
+
+function crazy() {
+    let container = document.querySelector('main');
+    
+
+    let row = document.createElement('div');
+    row.setAttribute('class', 'row');
+    console.log(row)
+
+    let colsCreate = colonneCrazy();
+    console.log(colsCreate);
+    row.innerHTML = colsCreate;
+    container.append(row);
+    console.log(row);
+    return container
+}
+
+
+
+
+const colCrazy = 49
+
+
+function colonneCrazy() {
+  let cols = '';
+  let numeriusati = [];
+  for(let i = 1; i <= colCrazy; i++){ 
+     cols += `
+    <div class="col-"> ${i}</div>
+    `;
+   }
+ return cols
+}
+
+
+
+
+
